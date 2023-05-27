ccab973ec5e9619eb50599fca8d4701931342752
Computer Acer <mariustucureanua@gmail.com>
2023-05-27 12:48:52 +0300

    first commint

---
+++ src/context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const AppContext = createContext();
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  return prefersDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState('cat');
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    // const body = document.querySelector('body');
+  };
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
